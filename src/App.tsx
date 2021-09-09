ece7bb7ee9a92a2a9495389883cac5b7af7acc39
Luiz Gustavo Lucio Pereira <74224127+gusta4065@users.noreply.github.com>
2021-09-09 00:16:11 -0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState, createContext} from 'react';
+import SideBar from "./components/sidebar/index"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  [array, setArray] = useState('');
+  function handleCreateArray( array: string ){
+      heapArray: number[] = 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="w3-container">
+
+      <label>Insira os valores</label>
+
+       <input className="w3-input" type="textarea"
+        onChange={context => context.setArray(event.target.value)}
+        value= {array}
+      />
+
+      {/*<button className="w3-button w3-round-xxlarge">and Rounder</button>*/}
+
+      </form>  
+      <main>
+        <div>
+          Rodrigo Lima
+        </div>
+      </main>
     </div>
   );
 }
